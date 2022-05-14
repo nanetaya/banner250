802beb6e37b1470c3025fa9e55375b8e996841f5
nanetaya <53016327+nanetaya@users.noreply.github.com>
2022-05-14 08:30:36 +0700

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,30 @@
+<div class="slideshow-container" align="center">
+
+<div class="mySlides fade">
+  <a href="https://invol.co/claq4g3" target="_blank">
+<img alt="tokopedia_affiliate" class="lazyload loaded" height="250" lazied="IMG" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNDV3q0S4Z2Ql0xdNG2prclzkJob-BtVoMWyYmrxQEOSm_bEz4sVk3BS5Sdl0SRzZPeXyvZcimBVm33ZamJGSlcg2JWK_inxRQOwqPMrudWNcSJcvV8yKK-YjC7IwD2mjI5xokGUG7SRBjxLYnskrOdJbiHEVFEfqlwRh3zQemz30FhWmOKWUzDg2OBg/s1600/tokopedia_affiliate.jpg
+" title="tokopedia_affiliate" width="250" />
+</a>
+</div>
+
+<div class="mySlides fade">
+  <a href="https://invl.io/claq4jp" target="_blank">
+<img alt="airasia_affiliate" class="lazyload loaded" height="250" lazied="IMG" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg2Ecs-xvJAPhF7_LU_4yez08dONXq_gs07KYBV0lLsDHuCQcKjVIV6fzAflIjTfDmogAgJYJApJvnlY4z1ieVUcLaZgx880Cg-qsTyTQAUpb8qkXfrYWPQir5Oo6gohqFR62RIf7tBPgJwV-2LQ23Wn-O-2LO47wL9CVer2fFa6icdpJB5JgFjoQEIow/s1600/airasia_affiliate.png" title="airasia_affiliate" width="250" />
+</a>
+</div>
+
+<div class="mySlides fade">
+  <a href="https://invol.co/claq4hp" target="_blank">
+<img alt="tokopedia_affiliate" class="lazyload loaded" height="250" lazied="IMG" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg1UPBrI8qR3r4gqgmO3_qCLiN4m0FINs1Efl3IrDtHHiNuaiguBPEjajdNmq1WzE9Dn0uZfmqKVU3ZnGqpaOcg7voX5mxhVtYAfLpz1dpZNUavCsx9hPinbYmNL9WQeeZrzm89gY7RSU9BvSUG90ZQpDkTyttg9P-n5f65SN0xf-5VvYgmLqBfOxtRzg/s1600/tokopedia_affiliate.jpg" title="tokopedia_affiliate" width="250" />
+</a>
+</div>
+
+</div>
+
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
